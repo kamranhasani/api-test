ba96fec81d82490edaa625edf7a483f937e3f538
kamran <kamranhasaniinfo@gmail.com>
2023-07-04 11:18:28 -0700

    .htaccess

--- helper.php
+++ helper.php
@@ -1,6 +1,7 @@
 <?php
+header('Content-Type: application/json; charset=utf-8');
 
-
+//------function-start------
 
 function test_input($data) {
     $data = trim($data);
@@ -10,5 +11,27 @@ function test_input($data) {
     return $data;
   }
 
+  function chektoken($value){
+    if(isset($_SESSION['token']) && $_SESSION['token']==$value)
+     unset($_SESSION['token']);
+      return true;
+  }
+
+  function curl($url){
+    $client = curl_init(); 
+    curl_setopt($client, CURLOPT_URL, $url);
+    curl_setopt($client,CURLOPT_RETURNTRANSFER,true); 
+    $response = curl_exec($client); 
+    curl_close($client);
+     return    $response;
   
+  }
+  
+  //-------function-ded--------
+
+
+
+
+
+  $url="http://127.0.0.1/api/api.php/".$gemail."/".$gname;
 ?>
